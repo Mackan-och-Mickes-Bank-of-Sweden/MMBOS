8e2effbb14d84cb02b4483594846104f0785f1e5
Michael <michael@graphicdesire.se>
2020-09-30 20:23:44 +0200

    App A

---
+++ App A/src/MMBOS/Main.java
@@ -0,0 +1,19 @@
+package MMBOS;
+
+public class Main {
+    public static void main(String[] args) {
+	// Skapa huvudmeny
+        String menuFormater = "%-10s %-25s\n";
+        System.out.println("       M & M Bank of Sweden       ");
+        System.out.format("%34s\n","----------------------------------");
+        System.out.format(menuFormater,"[1]", "Skapa konto");
+        System.out.format(menuFormater,"[2]", "Lista alla konton");
+        System.out.format(menuFormater,"[3]", "Insättningar");
+        System.out.format(menuFormater,"[4]", "Uttag");
+        System.out.format(menuFormater,"[5]", "Betalningsuppdrag (OCR)");
+        System.out.format(menuFormater,"[6]", "Banktillgångar");
+        System.out.format(menuFormater,"[7]", "Överföringar");
+        System.out.format("%34s\n","----------------------------------");
+        System.out.format(menuFormater,"[0]", "Avsluta programmet");
+    }
+}
